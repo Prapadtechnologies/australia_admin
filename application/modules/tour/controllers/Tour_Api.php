324f17d292a333faa945bde5422788f28d8401e8
G.V.Chakradhargit config --global user.name Your <you@example.com>
2024-03-23 10:32:32 +0530

    Added Tour and Trailer pages

---
+++ application/modules/tour/controllers/Tour_Api.php
@@ -0,0 +1,57 @@
+<?php
+require APPPATH . '/libraries/MY_REST_Controller.php';
+require APPPATH . '/vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+
+class Tour_Api extends MY_REST_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('user_model');
+    }
+
+    /**
+     * To get states and relatd details
+     *
+     * @author Mehar
+     * */
+
+
+    public function tour_list_get()
+    {
+        $this->validate_token($this->input->get_request_header('X_AUTH_TOKEN'));
+        $target = $_GET['q'];
+            //$where="lower('name') like '%".strtolower($target)."%'";
+        $data = $this->db->select('*')
+                ->order_by('created_at','desc')
+                ->get('tour')
+                ->result_array();
+        $this->set_response_simple(($data == FALSE) ? FALSE : $data, 'Success..!', REST_Controller::HTTP_OK, TRUE);
+    }
+    
+    public function tour_create_post()
+    {
+        $token_data = $this->validate_token($this->input->get_request_header('X_AUTH_TOKEN'));
+        $_POST = json_decode(file_get_contents("php://input"), TRUE);
+        /*$this->form_validation->set_rules($this->users_address_model->rules);
+        if ($this->form_validation->run() == false) {
+            $this->set_response_simple(validation_errors(), 'Validation Error', REST_Controller::HTTP_NON_AUTHORITATIVE_INFORMATION, FALSE);
+        } else {*/
+            $raw_data=[
+                "tour_name"=>$_POST['tour_name'],
+                "tour_type"=>$_POST['tour_type'],
+                "start_date"=>$_POST['start_date'],
+                "end_date"=>$_POST['end_date'],
+                "report_currency"=>$_POST['report_currency'],
+                "created_at"=>date('Y-m-d H:i:s'),
+                "created_by"=>$token_data->id
+            ];
+            $id = $this->db->insert('tour',$raw_data);
+            $this->set_response_simple($id, 'Success..!', REST_Controller::HTTP_CREATED, TRUE);
+        // }
+    }
+}
+

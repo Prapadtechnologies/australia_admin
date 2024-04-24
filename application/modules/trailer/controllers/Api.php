dc92a7d1badeb4ef7799ea2ed70dc962e9534e0e
G.V.Chakradhargit config --global user.name Your <you@example.com>
2024-04-24 20:24:44 +0530

    added trailer_delete_post API

--- application/modules/trailer/controllers/Api.php
+++ application/modules/trailer/controllers/Api.php
@@ -94,5 +94,40 @@ class Api extends MY_REST_Controller
         }
     }
 
+    public function trailer_delete_post($id)
+    {
+        $_POST = json_decode(file_get_contents("php://input"), TRUE);
+
+        // Fetch token data and validate if necessary
+        $token_data = $this->validate_token($this->input->get_request_header('X_AUTH_TOKEN'));
+
+        $existing_trailer_data = $this->db->get_where('trailer', array('id' => $id))->row_array();
+
+        if (!$existing_trailer_data) {
+            $this->set_response_simple("Trailer not found", 'Error..!', REST_Controller::HTTP_NOT_FOUND, FALSE);
+            return;
+        }
+
+        $updated_status  = array(
+            "user_id"=>$token_data->id,
+            "status" => isset($_POST['status']) ? $_POST['status'] : $existing_trailer_data['status'],//inactive
+            "updated_at" => date('Y-m-d H:i:s'),
+            "updated_by" => $token_data->id
+        );
+
+        // Update the trailer record in the database
+        $this->db->where('id', $id);
+        $this->db->update('trailer', $updated_status);
+
+        // Check if the update was successful
+        if ($this->db->affected_rows() > 0) {
+            $updated_trailer_status = $this->db->get_where('trailer', array('id' => $id))->row_array();
+            $this->set_response_simple(($existing_trailer_data == FALSE) ? [] : $existing_trailer_data, 'Success..!', REST_Controller::HTTP_OK, TRUE);
+            //$this->response($existing_trailer_data, REST_Controller::HTTP_OK);
+        } else {
+            $this->set_response_simple("Failed to delete trailer", 'Error..!', REST_Controller::HTTP_BAD_REQUEST, FALSE);
+        }
+    }
+
 }
 
